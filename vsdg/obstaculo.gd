68174f91c9b80ba82965867bf086c4acaa98ba73
Ezequiel Merino Cabrera <merinocabrera.ezequiel@gmail.com>
2025-11-08 15:58:44 +0100

    try to solve the speed encrease error

--- vsdg/obstaculo.gd
+++ vsdg/obstaculo.gd
@@ -8,14 +8,15 @@ var speed_multiplier = 1.0
 
 func _ready():
 	speed = 160.0
-	speed_multiplier = 1.0
+	#speed_multiplier = 1.0
 	add_to_group("obstaculos")
 	get_node("cono0"+str(Globalvars.playerSelect)).visible = true
 	get_node("CS_cono0"+str(Globalvars.playerSelect)).disabled = false
-	
+	print("process::> speed: "+ str(speed) + " - multiplier: " + str(speed_multiplier) + " - playerMultipier: " + str(Globalvars.playerMultiplier))
 
 func _process(delta):
 	if not Globalvars.gameover:
+		print("process::> speed: ", speed," - multiplier: ", speed_multiplier," - playerMultipier: ", Globalvars.playerMultiplier)
 		position.y += speed * speed_multiplier * Globalvars.playerMultiplier * delta
 		if position.y > get_viewport_rect().size.y + 50:
 			queue_free()
@@ -29,5 +30,5 @@ func setSpeed(_speed):
 	speed = _speed
 
 func new_speed_multiplier(_new_speed_multiplier):
-	#print("object increment speed")
+	print("============================================================================object increment speed: "+ str(_new_speed_multiplier))
 	speed_multiplier = _new_speed_multiplier
